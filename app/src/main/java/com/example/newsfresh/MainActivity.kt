16f8a3013a7c073d3e74a3951b9bb1308a26db8f
Simonne Pinto <pintosimonne@gmail.com>
2021-03-18 18:01:11 +0530

    load data from API

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.newsfresh
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
-    override fun onItemClicked(Item: String) {
-        Toast.makeText(this, "Clicked item is $Item", Toast.LENGTH_LONG).show()
+
+    private lateinit var mAdapter: NewsListAdapter
+
+    override fun onItemClicked(item: News) {
+        //Toast.makeText(this, "Clicked item is $Item", Toast.LENGTH_LONG).show()
+        val builder =  CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,16 +30,37 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
 
         //Recycler view requires a layout manager and adapter
         recyclerView.layoutManager = LinearLayoutManager(this)  //there are many layout managers for different outputs
-        val items = fetchData()
-        val adapter: NewsListAdapter = NewsListAdapter(items, this)
-        recyclerView.adapter = adapter
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
     }
 
-    private fun fetchData():ArrayList<String>{
-        val list = ArrayList<String>()
-        for(i in 0 until 100){
-            list.add("Item $i")
-        }
-        return list
+    private fun fetchData(){
+        val url="https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            Response.ErrorListener {
+
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 }
